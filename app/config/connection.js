edda23f77ce9c8d3e17660379acad81589558576
Ariya Wildan <ariyadevanto@gmail.com>
2024-11-21 14:40:16 +0700

    add db

--- app/config/connection.js
+++ app/config/connection.js
@@ -5,9 +5,9 @@ if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
+        host: 'dbk3.c10i4ai8mej0.us-east-1.rds.amazonaws.com',
+        user: 'admin',
+        password: 'kelompok-3',
         database: 'todos_db'
     });
 }
